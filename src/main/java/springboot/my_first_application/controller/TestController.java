b67d3d42e15df0912e0e20da5c17df210155213c
zraffai <raffai.89.zoltan@gmail.com>
2018-08-17 10:38:48 +0200

    html resources added

--- src/main/java/springboot/my_first_application/controller/TestController.java
+++ src/main/java/springboot/my_first_application/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-	@RequestMapping
+	@RequestMapping("/")
 	public String main() {
 		return "Spring boot is working!";	
 	}
